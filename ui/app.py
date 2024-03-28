583ed97ef0dd387a455d9364c6abbe4065c18c2a
Lily Knab <lknab@esperanto.ai>
2024-03-28 11:24:17 -0700

    removed files

--- ui/app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.header("Demo 1: Image Segmentation")
-st.text("Uses DeepLabV3 to obtain semantic segmentation maps of an input image. Example screenshot of demo below.")
-st.image("./photos/demo1.png")
-st.header("Demo 2 Name")
-st.text("Demo 1 description")
-
